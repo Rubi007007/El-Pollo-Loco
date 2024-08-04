8c56dcb85ef1447c4353bbc79d8b12be063967e6
Rubi007007 <151785420+Rubi007007@users.noreply.github.com>
2024-08-04 20:32:59 +0200

    char can only run to a specific point on the map, right and left

--- models/world.class.js
+++ models/world.class.js
@@ -1,5 +1,6 @@
 class World {
     character = new Character();
+    level = level1;
     enemies = level1.enemies;
     clouds = level1.clouds;
     backgroundObjects = level1.backgroundObjects;
@@ -26,9 +27,9 @@ class World {
 
         this.ctx.translate(this.camera_x, 0);
 
-        this.addObjectsToMap(this.backgroundObjects);
-        this.addObjectsToMap(this.clouds);
-        this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.level.backgroundObjects);
+        this.addObjectsToMap(this.level.clouds);
+        this.addObjectsToMap(this.level.enemies);
         this.addToMap(this.character);
 
         this.ctx.translate(-this.camera_x, 0);
