4b3ab02965c09b62327dc9c99f09fee935a96ef0
Rubi007007 <151785420+Rubi007007@users.noreply.github.com>
2024-08-04 19:25:28 +0200

    char can walk left and right with animation

--- models/character.class.js
+++ models/character.class.js
@@ -2,6 +2,7 @@ class Character extends MovableObject {
     height = 280;
     width = 140;
     y = 150;
+    speed = 10;
 
     IMAGES_WALKING = [
         './img/2_character_pepe/2_walk/W-21.png',
@@ -23,12 +24,23 @@ class Character extends MovableObject {
     animate() {
         setInterval(() => {
             if (this.world.keyboard.RIGHT) {
+                this.x += this.speed;
+            };
+
+            if (this.world.keyboard.LEFT) {
+                this.x -= this.speed;
+            };
+        }, 1000 / 60);
+
+        setInterval(() => {
+            if (this.world.keyboard.RIGHT || this.world.keyboard.LEFT) {
+                // walk animation
                 let i = this.currentImage % this.IMAGES_WALKING.length;
                 let path = this.IMAGES_WALKING[i];
                 this.img = this.imageCache[path];
                 this.currentImage++;
             }
-        }, 100);
+        }, 50);
     }
 
     jump() {}
