6c38bacebabf4d8d73bca298abfcc333a3f6ad04
Rubi007007 <151785420+Rubi007007@users.noreply.github.com>
2024-08-04 18:31:55 +0200

    chicken are moving now

--- js/models/movable-object.class.js
+++ js/models/movable-object.class.js
@@ -6,6 +6,7 @@ class MovableObject {
     img;
     imageCache = {};
     currentImage = 0;
+    speed = 0.15;
 
     loadImage(path) {
         this.img = new Image();
@@ -25,6 +26,8 @@ class MovableObject {
     }
 
     moveLeft() {
-        
+        setInterval(() => {
+            this.x -= this.speed;
+        }, 1000 / 60);
     }
 }
