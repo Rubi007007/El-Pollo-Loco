a50ba543093ec240d0910e9ccbc19d0872b5a54d
Rubi007007 <151785420+Rubi007007@users.noreply.github.com>
2024-08-04 07:58:07 +0200

    draw Funktion

--- js/models/world.class.js
+++ js/models/world.class.js
@@ -5,14 +5,27 @@ class World {
         new Chicken(),
         new Chicken(),
     ];
+    canvas;
     ctx;
 
     constructor(canvas) {
         this.ctx = canvas.getContext('2d');
+        this.canvas = canvas;
         this.draw();
     }
 
     draw() {
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height)
+        this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
+        enemies.forEach(enemy => {
+            this.ctx.drawImage(this.enemy.img, this.enemy.x, this.enemy.y, this.enemy.width, this.enemy.height);
+        });
+
+        // Draw() wird immer wieder aufgerufen
+        let self = this; // this wird innerhalb der u.a. Funktion nicht erkannt
+        requestAnimationFrame(function() {
+            self.draw();
+        })
     }
 }
