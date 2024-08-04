bbc4c589ab7f6c17529789fec386c7b865290902
Rubi007007 <151785420+Rubi007007@users.noreply.github.com>
2024-08-04 09:54:25 +0200

    Background eingefügt

--- js/models/world.class.js
+++ js/models/world.class.js
@@ -8,6 +8,9 @@ class World {
     clouds = [
         new Cloud()
     ];
+    backgroundObjects = [
+        new BackgroundObject('./img/5_background/layers/1_first_layer/1.png', 0, 80)
+    ];
     canvas;
     ctx;
 
@@ -17,23 +20,32 @@ class World {
         this.draw();
     }
 
+
+
+
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        this.ctx.drawImage(this.character.img, this.character.x, this.character.y, this.character.width, this.character.height);
-        
-        this.enemies.forEach(enemy => {
-            this.ctx.drawImage(enemy.img, enemy.x, enemy.y, enemy.width, enemy.height);
-        });
+        this.addToMap(this.character);
+        this.addObjectsToMap(this.clouds);
+        this.addObjectsToMap(this.enemies);
+        this.addObjectsToMap(this.backgroundObjects);
 
-        this.clouds.forEach(cloud => {
-            this.ctx.drawImage(cloud.img, cloud.x, cloud.y, cloud.width, cloud.height);
-        });
 
         // Draw() wird immer wieder aufgerufen
         let self = this; // this wird innerhalb der u.a. Funktion nicht erkannt
         requestAnimationFrame(function() {
             self.draw();
         })
+    }
+
+    addObjectsToMap(objects) {
+        objects.forEach(obj => {
+            this.addToMap(obj);
+        });
+    }
+
+    addToMap(mO) {
+        this.ctx.drawImage(mO.img, mO.x, mO.y, mO.width, mO.height);
     }
 }
