50e872ac6944d696690d6284e30967cf5c3042b0
Rubi007007 <151785420+Rubi007007@users.noreply.github.com>
2024-08-11 22:03:22 +0200

    Update throwable-object.class.js

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -8,10 +8,20 @@ class ThrowableObject extends MovableObject {
         './img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
         './img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png'
     ];
+
+    IMAGES_BOTTLE_SPLASH = [
+        './img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
+        './img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
+        './img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
+        './img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
+        './img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png',
+        './img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png',
+    ];
     
     constructor(x, y, otherDirection) {
         super().loadImage('./img/6_salsa_bottle/salsa_bottle.png');
         this.loadImages(this.IMAGES_BOTTLE_THROW);
+        this.loadImages(this.IMAGES_BOTTLE_SPLASH);
         this.x = x;
         this.y = y;
         this.width = 70;
@@ -28,11 +38,16 @@ class ThrowableObject extends MovableObject {
     throwRight() {
         this.speedY = 17;
         this.applyGravity();
-        this.animate();
-        setInterval(() => { // 117
-            this.x += 13;
-            if (this.y == 117) {
-                console.log('test')
+        this.animateThrowingBottle();
+        let intervalID = setInterval(() => {
+            if (this.y <= 350) {
+                this.x += 13;
+            } else {
+                clearInterval(intervalID);
+                this.speedY = 0;
+                this.acceleration = 0;
+                this.y = 350;
+                this.animateSplashedBottle(); // ToDo: STOPPING BOTH ANIMATIONS 
             }
         }, 25);
     }
@@ -40,15 +55,21 @@ class ThrowableObject extends MovableObject {
     throwLeft() {
         this.speedY = 8;
         this.applyGravity();
-        this.animate();
-        setInterval(() => { // 259
+        this.animateThrowingBottle();
+        setInterval(() => {
             this.x -= 10;
         }, 25);
     }
 
-    animate() {
+    animateThrowingBottle() {
         setInterval(() => {
             this.playAnimation(this.IMAGES_BOTTLE_THROW);
+        }, 1000 / 100);
+    }
+
+    animateSplashedBottle() {
+        setInterval(() => {
+            this.playAnimation(this.IMAGES_BOTTLE_SPLASH);
         }, 1000 / 100);
     }
 }
