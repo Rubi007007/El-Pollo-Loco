bc90e5537d9810392bbd364c449af0d614c55283
Rubi007007 <151785420+Rubi007007@users.noreply.github.com>
2024-08-11 21:08:06 +0200

    throw_bottle_sound

--- models/throwable-object.class.js
+++ models/throwable-object.class.js
@@ -1,5 +1,7 @@
 class ThrowableObject extends MovableObject {
 
+    throw_bottle_sound = new Audio('./audio/throw_bottle.mp3');
+
     IMAGES_BOTTLE_THROW = [
         './img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
         './img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
@@ -14,7 +16,8 @@ class ThrowableObject extends MovableObject {
         this.y = y;
         this.width = 70;
         this.height = 70;
-        
+        this.throw_bottle_sound.play();
+        this.throw_bottle_sound.volume = 0.4;
         if (otherDirection == false) {
             this.throwRight();
         } else if (otherDirection == true) {
