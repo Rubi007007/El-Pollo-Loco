3c8cdf21038d8cdc52810f3443a2fa8ddf3e9378
Rubi007007 <151785420+Rubi007007@users.noreply.github.com>
2024-08-27 10:41:10 +0200

    added mobile keys function

--- models/keyboard.class.js
+++ models/keyboard.class.js
@@ -5,4 +5,53 @@ class Keyboard {
     DOWN = false;
     SPACE = false;
     THROW = false;
+
+    constructor() {
+        this.btnPressEventsMobile();
+    }
+
+    btnPressEventsMobile() {
+        document.getElementById('arrow-btn-left-mobile').addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            this.LEFT = true;
+        });
+
+        document.getElementById('arrow-btn-left-mobile').addEventListener('touchend', (e) => {
+            e.preventDefault();
+            this.LEFT = false;
+        });
+
+        document.getElementById('arrow-btn-right-mobile').addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            this.RIGHT = true;
+        });
+
+        document.getElementById('arrow-btn-right-mobile').addEventListener('touchend', (e) => {
+            e.preventDefault();
+            this.RIGHT = false;
+        });
+
+        document.getElementById('jump-btn-mobile').addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            this.UP = true;
+        });
+
+        document.getElementById('jump-btn-mobile').addEventListener('touchend', (e) => {
+            e.preventDefault();
+            this.UP = false;
+        });
+
+        document.getElementById('throw-btn-mobile').addEventListener('touchstart', (e) => {
+            e.preventDefault();
+            this.THROW = true;
+        });
+
+        document.getElementById('throw-btn-mobile').addEventListener('touchend', (e) => {
+            e.preventDefault();
+            this.THROW = false;
+        });
+    }
 }
+
+
+
