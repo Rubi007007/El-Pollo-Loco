8d57d2f045633c8e569e6c2162f922b3351f15ab
Rubi007007 <151785420+Rubi007007@users.noreply.github.com>
2024-08-28 09:09:12 +0200

    JSDocs implemented to coins

--- models/coin.class.js
+++ models/coin.class.js
@@ -1,3 +1,8 @@
+/**
+ * Represents a Coin object in the game. Coins are collectible items that animate with a flipping effect.
+ * @class
+ * @extends MovableObject
+ */
 class Coin extends MovableObject {
     coins = 0;
     width = 60;
@@ -19,12 +24,21 @@ class Coin extends MovableObject {
         this.animate();
     }
 
+    /**
+     * Animates the coin by flipping between different images at a set interval.
+     * The animation is controlled by changing the image every 600 milliseconds.
+     */
     animate() {
         setInterval(() => {
             this.playAnimation(this.IMAGES_COINS);
         }, 600);
     }
 
+    /**
+     * Ensures that the coin's position does not collide with any existing coins.
+     * Repositions the coin until it is placed in a non-colliding location.
+     * @param {Array} existingCoins - Array of existing coin objects to check for collision.
+     */
     notCollidingCoins(existingCoins) {
         let isColliding;
         do {
