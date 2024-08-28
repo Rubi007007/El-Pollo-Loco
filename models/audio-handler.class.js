bb07a0c446d5679c09b7a6b2376f0464f3709dc9
Rubi007007 <151785420+Rubi007007@users.noreply.github.com>
2024-08-28 09:02:02 +0200

    JSDocs implemented to audio-handler

--- models/audio-handler.class.js
+++ models/audio-handler.class.js
@@ -1,3 +1,7 @@
+/**
+ * Handles audio control for the game, including muting, stopping, and managing sound volumes.
+ * @class
+ */
 class AudioHandler {
     isMuted;
     isStopped;
@@ -6,6 +10,12 @@ class AudioHandler {
         this.world = world;
     }
 
+    /**
+     * Toggles the volume of the specified sound. If the game is muted, the volume is set to 0.
+     * Otherwise, the volume is set to the specified value.
+     * @param {HTMLAudioElement} sound - The sound object whose volume will be adjusted.
+     * @param {number} volume - The volume level to set, between 0 and 1.
+     */
     toggleVolume(sound, volume) {
         if (!isMuted || isMuted == null) {
             sound.volume = volume;
@@ -14,6 +24,11 @@ class AudioHandler {
         }
     }
 
+    /**
+     * Toggles the sound playback based on the `isStopped` property. 
+     * If `isStopped` is true, the sound is paused; otherwise, it is played.
+     * @param {HTMLAudioElement} sound - The sound object to play or pause.
+     */
     toggleSound(sound) {
         if (this.isStopped) {
             sound.pause();
