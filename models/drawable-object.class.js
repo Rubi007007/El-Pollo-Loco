4c4fa20081f38875cab8186762113f857dc51041
Rubi007007 <151785420+Rubi007007@users.noreply.github.com>
2024-08-28 09:11:41 +0200

    JSDocs implemented to drawable-objects

--- models/drawable-object.class.js
+++ models/drawable-object.class.js
@@ -1,3 +1,7 @@
+/**
+ * Represents an object that can be drawn on the canvas.
+ * @class
+ */
 class DrawableObject {
     img;
     imageCache = {};
@@ -7,11 +11,18 @@ class DrawableObject {
     width = 100;
     height = 150;
 
-
+    /**
+     * Draws the object's image on the canvas.
+     * @param {CanvasRenderingContext2D} ctx - The canvas rendering context.
+     */
     draw(ctx) {
         ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
     }
 
+    /**
+     * Draws a collision frame around the object for debugging purposes.
+     * @param {CanvasRenderingContext2D} ctx - The canvas rendering context.
+     */
     drawCollisionFrame(ctx) {
         if (this instanceof Character || this instanceof Bottle || this instanceof Coin || this instanceof Chicken || this instanceof SmallChicken || this instanceof Endboss) {
             ctx.beginPath();
@@ -22,11 +33,19 @@ class DrawableObject {
         }
     }
 
+    /**
+     * Loads a single image from the specified path.
+     * @param {string} path - The path to the image file.
+     */
     loadImage(path) {
         this.img = new Image();
         this.img.src = path;
     }
 
+    /**
+     * Loads multiple images from the specified array of paths.
+     * @param {string[]} arr - An array of paths to image files.
+     */
     loadImages(arr) {
         arr.forEach((path) => {
             let img = new Image();
@@ -35,12 +54,21 @@ class DrawableObject {
         });
     }
 
+    /**
+     * Sets the object's image based on a percentage value, selecting an image from an array.
+     * @param {number} percentage - The percentage value used to determine the image.
+     * @param {string[]} array - An array of image paths to choose from.
+     */
     setPercentage(percentage, array) {
         this.percentage = percentage;
         let path = array[this.resolveImageIndex()];
         this.img = this.imageCache[path];
     }
 
+    /**
+     * Resolves the index of the image to use based on the percentage value.
+     * @returns {number} - The index of the image.
+     */
     resolveImageIndex() {
         if (this.percentage >= 100) {
             return 5
